e1768855d6bf05b7e5306da6f0ee8b01108c2421
lmislm <wszdlm@gmail.com>
2018-02-22 22:04:02 +0800

    router

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
     <!--<show-blogs></show-blogs>-->
     <!--<list-blogs></list-blogs>-->
@@ -11,12 +12,15 @@
   import addBlog from './components/addBlog.vue';
   import showBlogs from './components/showBlogs.vue';
   import listBlogs from "./components/listBlogs.vue";
+  import header from "./components/header.vue";
+  import single from "./components/singleBlog.vue";
 
   export default {
     components: {
       'add-blog': addBlog,
       'show-blogs': showBlogs,
       'list-blogs': listBlogs,
+      'app-header': header
     },
     data () {
       return {
--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -4,7 +4,9 @@
         <h1>博客文章</h1>
         <input type="text" v-model="search" placeholder="搜索 文章"/>
         <div v-for="blog in filteredBlogs" class="single-blog">
-          <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+          <router-link v-bind:to="'/blog/' + blog.id">
+            <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+          </router-link>
           <article>{{ blog.body | snippet }}</article>
         </div>
       </div>
